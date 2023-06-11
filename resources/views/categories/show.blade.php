cc568a470cef70c7e4c9465be5cdf28b6b8ea477
imancrrzii <imancarrazi777@gmail.com>
2023-06-11 11:34:01 +0700

    Create Category for customer

---
+++ resources/views/categories/show.blade.php
@@ -0,0 +1,31 @@
+<x-guest-layout>
+    <section class="mt-8 bg-white">
+        <div class="mt-4 text-center">
+            <h3 class="text-2xl font-bold">Our Menu</h3>
+            <h2 class="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">
+                TODAY'S SPECIALITY</h2>
+        </div>
+        <div class="container w-full px-5 py-6 mx-auto">
+            <div class="grid lg:grid-cols-4 gap-y-6">
+                @foreach ($category->menus as $menu)
+                    <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
+                        <img class="w-full h-48"
+                            src="{{ Storage::url($menu->image) }}"
+                            alt="Image" />
+                            <div class="px-6 py-4">
+                                <div class="flex mb-2">
+                                    <span class="px-4 py-0.5 text-sm bg-red-500 rounded-full text-red-50">{{ $category->name }}</span>
+                                </div>
+                                <h4 class="mb-3 text-xl font-semibold tracking-tight text-green-600 uppercase">{{ $menu->name }}</h4>
+                                <p class="leading-normal text-gray-700">{{ $menu->description}}</p>
+                            </div>
+                            <div class="flex items-center justify-between p-4">
+                                <button class="px-4 py-2 bg-green-600 text-green-50">Order Now</button>
+                                <span class="text-xl text-green-600">{{ $menu->price }}</span>
+                            </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </section>
+</x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -39,13 +39,13 @@
               <div :class="isOpen ? 'flex' : 'hidden'"
                 class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                  href="#">Home</a>
+                  href="{{ route('dashboard-customer') }}">Home</a>
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                  href="#">About Us</a>
+                  href="{{ route('categories.index' )}}">Category</a>
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                  href="#">Our Menu</a>
+                  href="{{ route('menus.index') }}">Our Menu</a>
                 <a class="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 hover:text-green-400"
-                  href="#">Make Reservation</a>
+                  href="{{ route('reservations.step.one') }}">Make Reservation</a>
               </div>
             </nav>
           </div>
